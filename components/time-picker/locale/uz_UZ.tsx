19b5501257a8a05faf4d5cd14712f0edfaf2cb1e
brian <brian@netquestsolutions.com>
2023-09-21 12:42:27 +0800

    Just the compiled

--- components/time-picker/locale/uz_UZ.tsx
+++
@@ -1,8 +0,0 @@
-import type { TimePickerLocale } from '../index';
-
-const locale: TimePickerLocale = {
-  placeholder: 'Vaqtni tanlang',
-  rangePlaceholder: ['Boshlanish vaqti', 'Yakunlash vaqti'],
-};
-
-export default locale;
